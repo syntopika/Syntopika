396dfa21d7cb33483808381013c4a71a940a0199
Bob Savage <syntopika@me.com>
2010-08-21 20:45:17 -0400

    refactoring (chgd def to let, and comments).

--- rdf_nt/rdf_write_server.clj
+++ rdf_nt/rdf_write_server.clj
@@ -10,29 +10,33 @@
 ;      wraps function in 1, and HTTP response doc.)" "\n" )
 ;
 
-(ns ^{:author "Bob Savage" :version "v0.1"}
+(ns ^{:author "Bob Savage" :version "v0.2"}
    rdf-nt.rdf-write-server
    ; imports & dependencies
+   (:use [clojure.java.io :only (reader writer)])
 )
 ;
-; 0 - rdf-statement creates [:subject :predicate :object]
+; 0.1 - rdf-statement creates [:subject :predicate :object]
 ;
 (defn rdf-statement "0 - rdf-statement creates [:subject :predicate :object]"
       [theSubject thePredicate theObject]
       {:subject theSubject :predicate thePredicate :object theObject})
 
+; 0.2 - statementSTRING coerce to n-triple string representation
 (defn statementSTRING "Convert an rdf-statement to n-triples (period terminated) string" [s] 
     (str (s :subject) " " (s :predicate) " " (s :object) " ."))
 ;
 ; 1 - create-statements (maps formData to a sequence rdf-statement, includes formatting).
 ;
+; 1.1 - initialize-statement-creator
+;
 (defn initialize-statement-creator
       "Pre-dondition: The calling context must have ensured that we have a fresh file at (re)start, else file corruption can occur. The serviceID used in naming the blank nodes that represent the service event is only guaranteed to be unique within this file. Using a file from a previous run can thus include duplicate serviceIDs for different service events."
       []
       (atom 0))
-;;
-;;  create-statements
-;;
+;
+;  1.2 - create-statements
+;
 ;     The statement creator must be initialized before calling create-statements!
 ;     
 ;     formData is:
@@ -65,38 +69,48 @@
     (if-not (number? @statement-creator)
         (throw (new java.lang.Exception "Invalid Statement Creator")))
     ;
-    (def id-num (deref statement-creator))
-    ; increment statemets-created
-    (swap! statement-creator + 1)
-
-    ; construct statements
-    ; statement 1
-    [(rdf-statement (str  "<mailto://" (:customerEMAIL formData) ">")
+    ;
+    (let [id-num (deref statement-creator)]
+      ; increment statemets-created
+      (swap! statement-creator + 1)
+      ;
+      ; construct statements
+      ;
+      ; statement 1
+      [(rdf-statement (str  "<mailto://" (:customerEMAIL formData) ">")
                     (str "<http://www.example.com/barbershop/Predicate#name>")
                     (:customerNAME formData))
-    ; statement 2
-     (rdf-statement (str "<mailto://" (:customerEMAIL formData) ">")
+      ; statement 2
+      (rdf-statement (str "<mailto://" (:customerEMAIL formData) ">")
                     (str "<http://www.example.com/barbershop/Predicate#received>")
                     (str "_:service" id-num))
-    ; statement 3
-     (rdf-statement (str "_:service" id-num )
+      ; statement 3
+      (rdf-statement (str "_:service" id-num )
                     (str "<http://www.example.com/barbershop/Predicate#cost>")
                     (:serviceCOST formData))
-    ; statement 4
-     (rdf-statement (str "_:service" id-num )
+      ; statement 4
+      (rdf-statement (str "_:service" id-num )
                     (str "<http://www.example.com/barbershop/Predicate#date>")
                     (:serviceDATE formData))
-    ; statement 5
-     (rdf-statement (str "_:service" id-num )
+      ; statement 5
+      (rdf-statement (str "_:service" id-num )
                     (str "<http://www.example.com/barbershop/Predicate#providedBy>")
                     (:barberID formData))]
+    ))
+
+;
+; 2.1 - nt-rdf-writer-clear?
+;
+(defn nt-rdf-writer-clear? "checks to make sure no existing file exists, else throws exception."
+    []
 )
 
 ;
-; 2 - nt-rdf-writer: takes all of the rdf statements and appends N-triples to file.
-;     Pre-condition: statements is assumed to be a sequence of simple-rdf-statement
-;     that has been previously defined. This section just combines them into a
-;     string representation that is spit to file.
+; 2.2 - nt-rdf-writer: takes all of the rdf statements and appends N-triples to file.
+;     Pre-condition: statements is assumed to be a sequence of type RDF statement
+;     as has been previously defined. This section just combines them into a
+;     string representation (by invoking statementSTRING) that is appended (with line end) to file.
+;
 ;(defn nt-rdf-writer
 ;  "nt-rdf-writer: takes all of the variables and appends N-triples to file."
 ;  [outPath statements]
